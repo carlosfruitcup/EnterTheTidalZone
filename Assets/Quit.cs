9b0296078fff9f71f1be6f0687832b1ded671fcf
nuppington-bit <harveyxthomison@gmail.com>
2020-06-16 19:04:27 -0700

    set up quit button
    umm

---
+++ Assets/Quit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void ExitApp(){
+		#if UNITY_EDITOR
+         // Application.Quit() is for built app
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+	}
+}
