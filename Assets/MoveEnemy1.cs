5c368b713e24eb5ea986d4e282d384a6653dcac7
nuppington-bit <harveyxthomison@gmail.com>
2020-06-17 14:18:40 -0700

    glowing stars, actual enemies (not working yet), and jenkins support for the android builds

---
+++ Assets/MoveEnemy1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveEnemy1 : MonoBehaviour
+{
+	public int distance;
+	private int tracker;
+	private int negative;
+	private bool lorr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		transform.position = new Vector3(1.0f, 1.0f, 1.0f);
+		tracker = distance;
+		negative = distance - distance * 2;
+		lorr = true;
+		transform.position = transform.position + new Vector3(-16, -15, -5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tracker == 0){
+			lorr = !lorr;
+			tracker = distance;
+		} else {
+			if(lorr){
+				transform.Translate(Vector3.right * distance);
+				tracker--;
+			} else {
+				transform.Translate(Vector3.right * negative);
+				tracker--;
+			}
+		}
+    }
+}
